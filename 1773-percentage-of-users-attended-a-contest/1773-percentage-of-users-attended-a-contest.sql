81b2423052d489b8a3cca4cd4d6c40b5d67c2b89
said sabry <120335918+saidsabry131@users.noreply.github.com>
2025-04-04 20:58:17 +0200

    Time: 2428 ms (98.48%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
-select r.contest_id ,
-round(CAST(count(r.user_id) as float)*100/(select count(*) from users) ,2)as percentage 
-	
-from Register r 
-group by r.contest_id
-order by round(CAST(count(r.user_id) as float)*100/(select count(*) from users) ,2) desc,r.contest_id 
+SELECT 
+    r.contest_id,
+    ROUND(((1.0 * COUNT(DISTINCT r.user_id)) / (SELECT COUNT(user_id) FROM Users) * 100), 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
