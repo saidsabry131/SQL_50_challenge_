dd92edebaba723fe656d0f8ce0bab754dcd5b367
said sabry <120335918+saidsabry131@users.noreply.github.com>
2025-04-04 20:52:09 +0200

    Time: 2950 ms (54.68%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -3,6 +3,5 @@ select r.contest_id ,
 round(CAST(count(r.user_id) as float)*100/(select count(*) from users) ,2)as percentage 
 	
 from Register r 
-left join USERS u on u.user_id =r.user_id 
 group by r.contest_id
-order by percentage desc,r.contest_id 
+order by round(CAST(count(r.user_id) as float)*100/(select count(*) from users) ,2) desc,r.contest_id 
