71c6e4dbf9a3ec4eb6fc449aaf5e97e0e238486e
said sabry <120335918+saidsabry131@users.noreply.github.com>
2025-04-29 15:21:22 +0300

    Time: 0 ms (100%), Space: 42 MB (89.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -3,9 +3,9 @@ class Solution {
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
-        while (j>=0)
+        while (i>=0&&j>=0)
         {
-            if(i >=0 && nums1[i] > nums2[j])
+            if( nums1[i] > nums2[j])
             {
 
                 nums1[k--]=nums1[i--];
@@ -19,6 +19,9 @@ class Solution {
             }
         }
 
-        
+        while(j>=0)
+        {
+            nums1[k--]=nums2[j--];
+        }
     }
 }
