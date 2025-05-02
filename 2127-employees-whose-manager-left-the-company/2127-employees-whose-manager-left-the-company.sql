d6400a70b4094e1e454ed45b1b8a9547731bf35d
said sabry <120335918+saidsabry131@users.noreply.github.com>
2025-05-02 19:39:45 +0300

    Time: 370 ms (88.06%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,12 +1,6 @@
-/* Write your T-SQL query statement below */
-
-select ee.employee_id
-from 
-employees ee 
-where ee.employee_id not in (
-
-	select e.employee_id  
+select e.employee_id  
 	from employees e
-	 join employees ms on  e.manager_id=ms.employee_id 
- ) and ee.salary <30000 and ee.manager_id is not null 
- order by ee.employee_id
+	left join employees ms on  e.manager_id=ms.employee_id 
+	where ms.employee_id is null and e.salary < 30000 and e.manager_id is not null
+
+    order by e.employee_id
