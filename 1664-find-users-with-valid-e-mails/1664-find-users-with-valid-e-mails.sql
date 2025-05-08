e118eafc964eaf8349023e4645c82f42c3b1f4e4
said sabry <120335918+saidsabry131@users.noreply.github.com>
2025-05-09 01:05:30 +0300

    Time: 623 ms (98.53%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
-select * from
-users where mail like '[A-z]%@leetcode.com'
-and mail not like '%[^A-z0-9_.-]%@leetcode.com'
+# Write your MySQL query statement below
+SELECT * 
+FROM users 
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$';
